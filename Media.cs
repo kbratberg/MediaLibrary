35558c94a8d796630c385eb84a48d27fd35cece2
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-09-30 18:50:26 -0500

    added scrub file

--- Media.cs
+++ Media.cs
@@ -35,16 +35,29 @@ namespace MediaLibrary
             return $"Id: {mediaId}\nTitle: {title}\nDirector: {director}\nRun time: {runningTime}\nGenres: {string.Join(", ", genres)}\n";
         }
 
-    }  
+    }
     // Album class is derived from Media class
     public class Album : Media
-        {
-            public string artist { get; set; }
-            public string recordLabel { get; set; }
+    {
+        public string artist { get; set; }
+        public string recordLabel { get; set; }
 
-            public override string Display()
-            {
-                return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
-            }
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nArtist: {artist}\nLabel: {recordLabel}\nGenres: {string.Join(", ", genres)}\n";
         }
+    }
+
+    // Book class is derived from Media class
+    public class Book : Media
+    {
+        public string author { get; set; }
+        public UInt16 pageCount { get; set; }
+        public string publisher { get; set; }
+
+        public override string Display()
+        {
+            return $"Id: {mediaId}\nTitle: {title}\nAuthor: {author}\nPages: {pageCount}\nPublisher: {publisher}\nGenres: {string.Join(", ", genres)}\n";
+        }
+    }
 }
