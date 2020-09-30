2965037adaa75d18d692cdb4bfbec1093749af01
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-09-30 18:40:11 -0500

    abstract

--- Program.cs
+++ Program.cs
@@ -5,13 +5,25 @@ using System.IO;
 namespace MediaLibrary
 {
     class Program
-    {  private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
+    {  
+        private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
         static void Main(string[] args)
         {
            logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
+        Movie movie = new Movie
+            {
+                mediaId = 123,
+                title = "Greatest Movie Ever, The (2020)",
+                  director = "Jeff Grissom",
+                // timespan (hours, minutes, seconds)
+                runningTime = new TimeSpan(2, 21, 23),
+                genres = { "Comedy", "Romance" }
+            };
+
+            Console.WriteLine(movie.Display());
+
 
             logger.Info("Program ended");
         }
